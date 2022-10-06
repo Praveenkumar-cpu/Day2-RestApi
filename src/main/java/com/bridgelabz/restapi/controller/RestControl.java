6f0335aca513742a578613fecd58ebc8215b6deb
praveen <praveenpkbadiger@gmail.com>
2022-10-06 17:55:37 +0530

    ADDED: [Praveen] Query Parameter call

--- src/main/java/com/bridgelabz/restapi/controller/RestControl.java
+++ src/main/java/com/bridgelabz/restapi/controller/RestControl.java
@@ -2,6 +2,7 @@ package com.bridgelabz.restapi.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class RestControl {
     @GetMapping("/home")
     public String home(){
         return "Hello from Bridgelab";
+    }
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello "+name+" From BridgeLabz";
     }
 
 }
