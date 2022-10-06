53477284df849ac0a865bbb4b4315ea5a21a5b6d
praveen <praveenpkbadiger@gmail.com>
2022-10-06 18:00:00 +0530

    ADDED:[Praveen]Path variable call

--- src/main/java/com/bridgelabz/restapi/controller/RestControl.java
+++ src/main/java/com/bridgelabz/restapi/controller/RestControl.java
@@ -2,6 +2,7 @@ package com.bridgelabz.restapi.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class RestControl {
 
     @GetMapping("/query")
     public String sayHello(@RequestParam String name) {
+        return "Hello "+name+" From BridgeLabz";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
         return "Hello "+name+" From BridgeLabz";
     }
 
